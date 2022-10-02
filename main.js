f9a79ed50b2720435adc943e5492f48fde6b4560
Summer <swatson9008@yahoo.com>
2022-10-02 11:50:53 -0400

    wrote up the pseudocode plans

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// write functions that can add, subtract, multiply and divide from the console log
+
+// create the operate function to take the operator and two numbers and call one of the math functions
+
+// create a function that displays the numbers typed on the text box when typed
+
+// wire the functions to the html calculator
