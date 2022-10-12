63369dab941f21d794fb09f7049eab870140af5c
Summer <swatson9008@yahoo.com>
2022-10-12 12:57:57 -0400

    more css adjustments

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-/// Variables 
+///Variables 
 
 const displayScreen = document.getElementById("displayNum");
 const buttons = document.getElementsByClassName("numbutton");
@@ -13,7 +13,18 @@ let outputS;
 let outputFinal;
 let outputF;
 
-///object for numbers and operators
+//Calculate Listeners
+
+calcB = document.getElementById("CalcS");
+calcB.addEventListener("click", calcArray);
+
+//Clear Screen Listeners
+
+ClearS = document.getElementById("ClearScreen");
+ClearS.addEventListener("click", ClearScreens);
+ClearS.addEventListener("keyup", ClearScreens);
+
+///Object to Store Numbers and Operators
 
 let calcStorage = {
     firstNum: "",
@@ -23,13 +34,8 @@ let calcStorage = {
 
 };
 
-//calculate listeners
 
-calcB = document.getElementById("CalcS");
-calcB.addEventListener("click", calcArray);
-
-
-///loop for button listeners
+//Loop for Button Listeners
 
 for (let i = 0; i < length; i++){
     
@@ -39,14 +45,8 @@ for (let i = 0; i < length; i++){
 
 }
 
-//clear screen listeners
 
-ClearS = document.getElementById("ClearScreen");
-ClearS.addEventListener("click", ClearScreens);
-ClearS.addEventListener("keyup", ClearScreens);
-
-
-
+//function that handles the numbers and operators pressed and calls to calculate the values
 
 function handle (event) {
     let value = event.target.value;
@@ -99,6 +99,8 @@ function handle (event) {
 }
 
 
+//function that puts the values in correct positions and calls the operate function
+
 function calcArray (){
     let firstNo = parseFloat(calcStorage.firstNum);
     let secondNo = parseFloat(calcStorage.secondNum);
@@ -130,7 +132,7 @@ function calcArray (){
 
 }
 
-///parsing floating numbers
+///Parses Floating Numbers
 
 function ParseFloat(str,val) {
     str = str.toString();
@@ -138,7 +140,7 @@ function ParseFloat(str,val) {
     return Number(str);   
 };
 
-// addition
+//addition
 function addOP(a, b){
     sumTotal = a+b;
     return sumTotal;
@@ -147,7 +149,7 @@ function addOP(a, b){
 
 console.log(addOP(1,2));
 
-// subtraction
+//subtraction
 function subOP(a, b){
     subTotal = a-b;
     return subTotal;
@@ -155,7 +157,7 @@ function subOP(a, b){
 
 console.log(subOP(9,4));
 
-// multiplication
+//multiplication
 function multiOP(a, b){
     multiTotal = a*b;
     return multiTotal;
@@ -163,7 +165,7 @@ function multiOP(a, b){
 
 console.log(multiOP(6,6));
 
-// division
+//division
 function divideOP(a, b){
     divideTotal = a / b;
     return divideTotal;
@@ -172,6 +174,7 @@ function divideOP(a, b){
 console.log(divideOP(30,2));
 
 
+//function that decides what operation to call
 function operate(a, b, c){
 
     switch(c){
@@ -206,156 +209,3 @@ function ClearScreens(){
         secondOP: "",
     };
 }
-
-/*
-
-function handle (event) {
-    let value = event.target.value;
-    
-    switch (value){
-    case '+':
-    case '-':
-    case '*':
-    case '/':
-    case '=':
-        if (calcStorage.firstOP === ""){calcStorage.firstOP += value;}
-        else {calcStorage.secondOP += value;}
-    break;
-        default:
-            if (calcStorage.firstOP === ""){calcStorage.firstNum += value;}
-            else if (calcStorage.secondOP === ""){calcStorage.secondNum += value;}
-            else {calcStorage.thirdNum += value;}
-            /*if (calcStorage.operatorS == "+" || "-" || "*" || "/"){calcStorage.secondNum += value;}
-            else {calcStorage.firstNum += value;} */ /*
-        }
-    
-        /*if (value === "+" || "-" || "*" || "/"){calcStorage.operatorS += value;}
-        else if (calcStorage.operatorS === "+" || "-" || "*" || "/"){calcStorage.secondNum += value;}
-        /*else {calcStorage.firstNum += value;}
-        displayScreen.value += value;
-        console.log(calcStorage);
-    }
-    
-
-function calcArray(){
-    let firstNo = parseInt(calcStorage.firstNum);
-    let secondNo = parseInt(calcStorage.secondNum);
-    let thirdNo = parseInt(calcStorage.thirdNum);
-    let firstOPs = calcStorage.firstOP;
-    let secondOPs = calcStorage.secondOP;
-    if (firstOPs === "*" || "/"){
-        firstOutput = operate (firstNo, secondNo, firstOPs);
-        secondOutput = operate (firstOutput, thirdNo, secondOPs);
-        displayScreen.value = secondOutput;
-        console.log(secondOutput);
-    }
-    else if (secondOPs === "*" || "/"){
-        firstOutput = operate (secondNo, thirdNo, secondOPs);
-        secondOutput = operate (firstNo, firstOutput, firstOPs);
-        displayScreen.value = secondOutput;
-        console.log(secondOutput);
-    }
-    else if (thirdNo === ""){
-        firstOutput = operate (firstNo, secondNo, firstOPs);
-        displayScreen.value = firstOutput;
-        console.log(firstOutput);
-    
-    }
-    else {
-        firstOutput = operate (firstNo, secondNo, firstOPs);
-        secondOutput = operate (firstOutput, thirdNo, secondOPs);
-        displayScreen.value = secondOutput;
-        console.log(secondOutput);
-    }
-    /*operate(a, b, calcStorage.firstOP);*/
-    /*displayScreen.value = ParseFloat(finalOutput, 2);
-    console.log(finalOutput);*/
-    
-
-
-
-/*function calcArray() {
-  console.log("hi");
-  /*console.log(calcStorage);
-  console.log(calcStorage.firstNum);
-  // console.log(operate());
-
-/*calcB.addEventListener("click", (e) => 
-e.preventDefault(), calcArray());
-
-function calcArray(){
-    
-    console.log(calcStorage.firstNum);
-};*/
-
-
-
-/*const opButtons = document.querySelectorAll(".operatorButtons")
-const opLength = opButtons.length;*/
-
-//for operators
-
-/*for (let i = 0; i < length; i++){
-    
-    opButtons[i].addEventListener("click", handleOP);
-    opButtons[i].addEventListener("keyup", handleOP);
-
-}*/
-
-/*function handleOP (event) {
-    const value = event.target.value;
-        if (displayScreen.value = ""){
-            alert("enter a number first")
-        }
-        else {
-    displayScreen.value += value
-    calcStorage.push(event.target.value);
-    console.log(calcStorage);};
-}*/
-
-//clear function
-
-/*for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", addNums(){
-        displayScreen.value += this.value;
-    });
-}
-
-
-
-
-/*
-
-const buttons1 = document.getElementById("No1");
-
-const buttons2 = document.getElementById("No2");
-
-buttons1.addEventListener("click", e=> {
-    e.preventDefault();
-    displayScreen.value = e.target.value;}
-    );
-
-buttons2.addEventListener("click", e=> {
-        e.preventDefault();
-        displayScreen.value = e.target.value;}
-        );
-
-/*for (let i=0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", e=> {
-        e.preventDefault();
-        displayScreen.value = e.target.value;
-    })
-}
-
-/*No1 = document.getElementById("No1").addEventListener("click", () => {
-    displayScreen.insertAdjacentText('afterbegin', 1);
-});
-
-
-/*function displayInput(x){
-displayScreen.insertAdjacentHTML(afterbegin.value, x);
-     };
-
-
-
-*/ 
